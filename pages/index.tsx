06565c78cab1318132f1761d8e863d0d1f6f1a9d
winalife-kp <137119640+winalife-kp@users.noreply.github.com>
2023-07-11 01:43:32 +0200

    1 file modified

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,11 @@
 import Head from 'next/head';
+import { useContext } from 'react';
+import ThemeModeContext from '../context/ThemeModeContext';
 import styles from '../styles/Home.module.css';
 
 export default function Home() {
+  const { handleToggleThemeMode, themeMode } = useContext(ThemeModeContext);
+
   return (
     <div className={styles.container}>
       <Head>
@@ -12,6 +16,11 @@ export default function Home() {
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
+        <div>
+          <button onClick={handleToggleThemeMode} color="inherit">
+            {`Switch to ${themeMode === 'dark' ? 'light' : 'dark'} mode`}
+          </button>
+        </div>
       </main>
     </div>
   );
